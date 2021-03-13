be965641518cbce821785c0ffb498dfde4483b66
Rajani Chaudhari <79529039+rajanic4717@users.noreply.github.com>
2021-03-13 14:44:28 +0530

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,30 @@
+import random
+from questions import question
+
+correct_answer={"Delhi",'28',"Aryabhatta","Mitochondria","26","Jaipur",
+            "Russia","Peacock","64","Sundar pichai"}
+quiz=dict(zip(question,correct_answer))
+
+print("RULES OF GAME")
+print("The first initial must be a capital letter of answer")
+print("Add a space between two words")
+score=0
+count=0
+l=set()
+while count<10:
+	x=random.randint(1,len(question)+1)
+	y=question[x]
+	print(y)
+	count+=1
+	your_answer=input("Enter The Correct Answer")
+if your_answer == quiz[y]:
+	score+=1
+	print('correct answer')
+	print("play again")
+	print("current score is:",score)
+else:
+	print("current score is:",score)
+	print('BETTER LUCK NEXT TIME')
+	print("play again")
+
+

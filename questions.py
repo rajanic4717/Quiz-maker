075fdb887cb7bdd3fba9b96ea67c3b1b3914e028
Rajani Chaudhari <79529039+rajanic4717@users.noreply.github.com>
2021-03-13 14:44:50 +0530

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,4 @@
+question=["what is capital of INDIA ?","how many states are their in INDIA ?","who invented zero ?",
+      "what is powerhouse of cell called?",'how many consonants are their in english language ?',
+      "which city is called pink city ?","largest contry in world","National bird of India ?","How many boxes are thier in chess board ?"
+      ,'ceo of google ?']
